45d76f89d2a4316df8c9d1363d733ffe916de44b
faizrehman1 <faizkhanabid@yahoo.com>
2017-02-03 01:53:14 +0500

    add RxJava Factory Adapter..

--- app/src/main/java/com/example/faizrehman/retrofit_example/MainActivity.java
+++ app/src/main/java/com/example/faizrehman/retrofit_example/MainActivity.java
@@ -9,8 +9,16 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Hello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,42 +26,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
+
+
         Services interfacess = Services.retrofit.create(Services.class);
-        Call<List<UserModel>> usermodels = interfacess.usermodels();
-        usermodels.enqueue(new Callback<List<UserModel>>() {
-            @Override
-            public void onResponse(Call<List<UserModel>> call, Response<List<UserModel>> response) {
-                if (response.isSuccessful()) {
-                    for (int i = 0; i < response.body().size(); i++) {
-                        Log.d("TAG", response.body().get(i).toString());
-                    }
-                }
-            }
+       // with Java
 
-            @Override
-            public void onFailure(Call<List<UserModel>> call, Throwable t) {
+//        Call<List<UserModel>> usermodels = interfacess.usermodels();
+//        usermodels.enqueue(new Callback<List<UserModel>>() {
+//            @Override
+//            public void onResponse(Call<List<UserModel>> call, Response<List<UserModel>> response) {
+//                if (response.isSuccessful()) {
+//                    for (int i = 0; i < response.body().size(); i++) {
+//                        Log.d("TAG", response.body().get(i).toString());
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<UserModel>> call, Throwable t) {
+//
+//            }
+//        });
 
-            }
-        });
+
+        //with RxJava
+        Observable<UserModel> userModelObservable = interfacess.getUser();
+       userModelObservable.subscribeOn(Schedulers.newThread())
+               .observeOn(AndroidSchedulers.mainThread())
+               .subscribe(new Observer<UserModel>() {
+                              @Override
+                              public void onCompleted() {
+
+                              }
+
+                              @Override
+                              public void onError(Throwable e) {
+
+                              }
+
+                              @Override
+                              public void onNext(UserModel userModel) {
+                                  Log.d("TAG",userModel.getEmail());
+                              }
+                          });
 
 
-        //for Single index of user
-        Services singleinterface = Services.getRetrofit.create(Services.class);
-       //
-        Call<UserModel> userModelCall = singleinterface.singleUser(2);
-        userModelCall.enqueue(new Callback<UserModel>() {
-            @Override
-            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
-                if(response.isSuccessful()){
-                    Log.e("TAG", response.body().getEmail());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<UserModel> call, Throwable t) {
-
-            }
-        });
+                       //for Single index of user
+//                       Services singleinterface = Services.getRetrofit.create(Services.class);
+//       //
+//        Call<UserModel> userModelCall = singleinterface.singleUser(2);
+//        userModelCall.enqueue(new Callback<UserModel>() {
+//            @Override
+//            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+//                if(response.isSuccessful()){
+//                    Log.e("TAG", response.body().getEmail());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<UserModel> call, Throwable t) {
+//
+//            }
+//        });
 
 
     }
--- app/src/main/java/com/example/faizrehman/retrofit_example/Services.java
+++ app/src/main/java/com/example/faizrehman/retrofit_example/Services.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import rx.Observable;
 
 /**
  * Created by faizrehman on 01/02/2017.
@@ -13,10 +15,12 @@ import retrofit2.http.GET;
 
 public interface Services {
     @GET("users/")
-    Call<List<UserModel>> usermodels();
-
-    Retrofit retrofit = new Retrofit.Builder().baseUrl(Urls.Base_url)
+  //  Call<List<UserModel>> usermodels();
+  Observable<UserModel> getUser();
+    Retrofit retrofit = new Retrofit.Builder()
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Urls.Base_url)
             .build();
 
 
